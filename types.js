8112531a551d5946045a19e379ab9c1b382d99bf
Roman Chaban <roman.chaban1001@gmail.com>
2024-02-26 19:13:20 +0200

    Initial Commit

---
+++ types.js
@@ -0,0 +1,4 @@
+var userName = 'Roman';
+var number = 23;
+var someNumbers = [1, 2, 3, 4, 5];
+console.log(someNumbers);
---
+++ types.ts
@@ -0,0 +1,8 @@
+const isFetching: boolean = true;
+const isLoading: boolean = false;
+
+
+const int: number = 23;
+const float: number = 2.3;
+const num: number = 3e10;
+

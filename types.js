0ea06f79717188e52ef61223782afb6aa71aa085
Roman Chaban <roman.chaban1001@gmail.com>
2024-02-26 19:27:51 +0200

    types learn

--- types.js
+++ types.js
@@ -1,4 +1,27 @@
-var userName = 'Roman';
-var number = 23;
-var someNumbers = [1, 2, 3, 4, 5];
-console.log(someNumbers);
+var isFetching = true;
+var isLoading = false;
+var int = 23;
+var float = 2.3;
+var num = 3e10;
+var myDogName = 'Ricky';
+var myDogHasLegs = 4;
+myDogHasLegs = 'Has 4 legs';
+var someMessage = 'Hello TypeScript';
+var numberArray = [1, 2, 3, 4, 5, 6];
+var tuplesArray = [10, 20, 30, 40, 50];
+var someArr = ['Hello', 23, true];
+var variable = 100;
+variable = 'New String';
+console.log(variable);
+var showMyName = function (userName) {
+    console.log(userName);
+};
+showMyName('Roman');
+var throwMessage = function (message) {
+    for (var i = 0; i < 10; i++) {
+        if (i <= 5) {
+            throw new Error(message);
+        }
+    }
+};
+throwMessage('Loops Error');
--- types.ts
+++ types.ts
@@ -1,8 +1,33 @@
 const isFetching: boolean = true;
 const isLoading: boolean = false;
 
-
 const int: number = 23;
 const float: number = 2.3;
 const num: number = 3e10;
+
+const myDogName: string = 'Ricky';
+let myDogHasLegs: any = 4;
+
+myDogHasLegs = 'Has 4 legs';
+
+const someMessage: string = 'Hello TypeScript';
+
+const numberArray: number[] = [1, 2, 3, 4, 5, 6];
+
+const tuplesArray: Array<number> = [10, 20, 30, 40, 50];
+
+const someArr: [string, number, boolean] = ['Hello', 23, true];
+
+let variable: any = 100;
+
+variable = 'New String';
+
+console.log(variable);
+
+const showMyName = (userName: string): void => {
+  console.log(userName);
+};
+
+showMyName('Roman');
+
 
